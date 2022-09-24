420a3c14a393705a5c7445421a3c88e7a59fcfac
Richieid23 <fitrarizki23@gmail.com>
2022-09-24 17:51:23 +0700

    add middleware verify token JWT

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const mediaRouter = require('./routes/media');
 const coursesRouter = require('./routes/courses');
 const paymentsRouter = require('./routes/payments');
 
+const verifyToken = require('./middlewares/verifyToken');
+
 const app = express();
 
 app.use(logger('dev'));
