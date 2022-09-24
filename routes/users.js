93d10713d05896c3c9b6b23a6d963eed2d02e898
Richieid23 <fitrarizki23@gmail.com>
2022-09-24 18:53:22 +0700

    add service user integration - user detail

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,7 @@ const verifyToken = require('../middlewares/verifyToken');
 router.post("/register", usersHandler.register);
 router.post("/login", usersHandler.login);
 router.put("/update", verifyToken, usersHandler.update);
+router.get("/", verifyToken, usersHandler.getUser);
 
 
 module.exports = router;
