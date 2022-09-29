616febf165c33aba45cc82916b96c7232a02cc5f
Richieid23 <fitrarizki23@gmail.com>
2022-09-29 15:35:23 +0700

    add service courses integration - lessons API

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const paymentsRouter = require('./routes/payments');
 const refershTokensRouter = require("./routes/refreshTokens");
 const mentorsRouter = require('./routes/mentors');
 const chaptersRouter = require("./routes/chapters");
+const lessonsRouter = require("./routes/lessons");
 
 const verifyToken = require('./middlewares/verifyToken');
 
@@ -31,5 +32,6 @@ app.use('/payments', paymentsRouter);
 app.use("/refresh-tokens", refershTokensRouter);
 app.use('/mentors', verifyToken, mentorsRouter);
 app.use("/chapters", verifyToken, chaptersRouter);
+app.use("/lessons", verifyToken, lessonsRouter);
 
 module.exports = app;
